473159ffd822c6910daff0594de0e772c159a401
GitHub Action <action@github.com>
2021-09-26 06:47:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/yfwfairy/mvitemplateplugin/MyBundle.kt
+++ src/main/kotlin/com/github/yfwfairy/mvitemplateplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.yfwfairy.mvitemplateplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/yfwfairy/mvitemplateplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.yfwfairy.mvitemplateplugin.services
+
+import com.github.yfwfairy.mvitemplateplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
